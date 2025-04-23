958f94c98dee5de8ae769abc038a70fc96a9c47a
Benjamin So <benso10@uw.edu>
2025-04-22 18:57:28 -0700

    pr test

---
+++ src/components/test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+  <div>
+  <nav>
+    test2
+    <div>
+        <h1 style={{fontFamily: 'Georgia, serif'}}>Happy Nails</h1>
+    </div>
+  </nav>
+  </div>
+  
+)
+
+}
+
+export default NavBar;
