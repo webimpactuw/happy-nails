f15d3e1bd6f68ced9f89f2a2a02153faadd50372
johnnynguynnn <dtn24@uw.edu>
2025-04-22 19:06:47 -0700

    navBar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import NavBar from '../components/navbar';
 
 function HomePage() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCount(prevCount => prevCount + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }
-  , []);
 
   return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Counter App</h1>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>
-        Increment
-      </button>
-    </div>
+    <>
+      <NavBar/>
+    </>
+
   );
 }
 
