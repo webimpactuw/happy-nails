cae45a0593d96922678aac4247123afad5709d89
Benjamin So <benso10@uw.edu>
2025-05-27 17:23:39 -0700

    nav bar and gallery

--- src/components/SlidingGallery.js
+++ src/components/SlidingGallery.js
@@ -6,46 +6,66 @@ import img1 from '../assets/pinklilynail.jpg';
 import img2 from '../assets/purpleflowernail.jpg';
 import img3 from '../assets/purplesharpnail.jpg';
 import img4 from '../assets/HeroBanner.png';
+import img5 from '../assets/pinkheartnail.png'; 
+import img6 from '../assets/greenpinknails.png'; 
 
-const images = [img1, img2, img3, img4];
+import arrow from '../assets/arrowdown.svg'; 
+
+const images = [img1, img2, img3, img4, img5, img6,img1, img2, img3, img4, img5, img6];
 
 function CustomNavigationOverlay() {
   const swiper = useSwiper();
   return (
-    <>
+  <>
       <button
         onClick={() => swiper.slidePrev()}
         style={{
           position: 'absolute',
           top: '50%',
           left: 0,
-          transform: 'translateY(-50%)',
+          transform: 'translateY(-50%) rotate(90deg)', // flipped for previous
           zIndex: 10,
-          background: 'rgba(0,0,0,0.4)',
-          color: '#fff',
+          background: 'transparent',
           border: 'none',
-          padding: '2em',
+          padding: '1em',
           cursor: 'pointer',
         }}
       >
+        <img
+          src={arrow}
+          alt="Previous"
+          style={{
+            width: '30px',
+            height: '30px',
+          }}
+        />
       </button>
+
       <button
         onClick={() => swiper.slideNext()}
         style={{
           position: 'absolute',
           top: '50%',
           right: 0,
-          transform: 'translateY(-50%)',
+          transform: 'translateY(-50%) rotate(-90deg)',
           zIndex: 10,
-          background: 'rgba(0,0,0,0.4)',
-          color: '#fff',
+          background: 'transparent',
           border: 'none',
-          padding: '2em',
+          padding: '1em',
           cursor: 'pointer',
         }}
       >
+        <img
+          src={arrow}
+          alt="Next"
+          style={{
+            width: '30px',
+            height: '30px',
+          }}
+        />
       </button>
     </>
+    
   );
 }
 
@@ -56,50 +76,50 @@ function SlidingGallery() {
         position: 'relative',
         width: '100%',
         overflow: 'hidden',
-        paddingBottom: '0px',
-        backgroundColor: '#f9c4d2'
+        backgroundColor: '#f9c4d2',
+        padding: '0 5vw',
+        boxSizing: 'border-box',
       }}
     >
       <Swiper
-        slidesPerView={3}
+        slidesPerView={5}
         loop={true}
         autoplay={{ delay: 3000, disableOnInteraction: false }}
         speed={600}
       >
         {images.map((img, index) => (
           <SwiperSlide key={index}>
-          <div
-            style={{
-              width: '100%',
-              aspectRatio: '4 / 3',
-              padding: '1em',
-              boxSizing: 'border-box',
-            }}
-          >
             <div
               style={{
                 width: '100%',
-                height: '100%',
-                borderRadius: '12px',
-                overflow: 'hidden',
+                aspectRatio: '1/1',
+                padding: '0.5em',
                 boxSizing: 'border-box',
               }}
             >
-              <img
-                src={img}
-                alt={`Slide ${index + 1}`}
+              <div
                 style={{
                   width: '100%',
                   height: '100%',
-                  objectFit: 'cover',
-                  display: 'block',
-                  borderRadius: '8px',
+                  borderRadius: '10px',
+                  overflow: 'hidden',
+                  boxSizing: 'border-box',
                 }}
-              />
+              >
+                <img
+                  src={img}
+                  alt={`Slide ${index + 1}`}
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover',
+                    display: 'block',
+                    borderRadius: '8px',
+                  }}
+                />
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        
+          </SwiperSlide>
         ))}
         <CustomNavigationOverlay />
       </Swiper>
